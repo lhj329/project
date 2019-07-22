a5fe4aacf2498348e613edc4b13c0f701a66e0a6
lhj329 <53169301+lhj329@users.noreply.github.com>
2019-07-22 18:54:19 +0900

    first

---
+++ ab/script/new.js
@@ -0,0 +1,22 @@
+/*let a =3;
+let b ='hi';
+let object={
+    c:'friends'
+};
+
+const string2 = `${b}, my ${a} ${object.c}`;
+
+function test(name,age){
+    console.log(`hello ${name} you are ${age} years old`);
+}
+
+test("HJ",20);
+*/
+
+var a = 3;
+var b = 'hi';
+var object = {
+  c: 'friends'
+};
+
+var string = b + ', my ' + a + ' ' + object.c; // 'hi, my 3 friends'
---
+++ ab/script/script.js
@@ -0,0 +1,27 @@
+var rside = document.getElementById("rside_cover")
+rside.addEventListener("click",function(e){
+    var toggle = document.getElementById("mbtn")
+    toggle.checked=false;
+})
+
+var lside = document.getElementById("lside_cover")
+lside.addEventListener("click",function(e){
+    var toggle =document.getElementById("lang");
+    toggle.checked=false;
+})
+
+  function hidden(){
+  var obj = document.body;
+  obj.style.overflow="hidden";
+ }
+
+
+//if(rside==true){
+//    document.getElementById("body")
+//    body.style.overflow = "hidden"
+//}
+//var hidden =document.getElementById("body")
+//
+//if(document.getElementById("mbtn")==true){
+//    hidden.style.overflow="hidden"
+//}
